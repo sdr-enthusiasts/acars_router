e93c315f74355ca41bf79aad52b692fcb9e4b930
Mike Nye <mike.nye@gmail.com>
2022-02-14 15:59:04 +0800

    implement command line args / env vars for config

--- acars_router/acars_router.py
+++ acars_router/acars_router.py
@@ -1,11 +1,48 @@
 #!/usr/bin/env python3
 
+import os
+import argparse
 import socket
-from twisted.logger import Logger
+import logging
 from twisted.internet.protocol import DatagramProtocol
 from twisted.internet import reactor
+from twisted.internet import task
 
-log = Logger()
+logging.basicConfig(level=logging.DEBUG)
+log = logging.getLogger('acars_router')
+
+# Globals: Counters
+COUNTER_ACARS_MSGS_RX_TOTAL = 0
+COUNTER_VDLM2_MSGS_RX_TOTAL = 0
+COUNTER_ACARS_MSGS_RX_LAST = 0
+COUNTER_VDLM2_MSGS_RX_LAST = 0
+
+def log_counters(trigger_in: int):
+    """
+    Logs the status of the global counters, then calls itself 5 minutes in the future.
+
+    trigger_in: int
+        Number of seconds into the future to trigger this function again
+    """
+    global COUNTER_ACARS_MSGS_RX_LAST
+    global COUNTER_ACARS_MSGS_RX_TOTAL
+    global COUNTER_VDLM2_MSGS_RX_LAST
+    global COUNTER_VDLM2_MSGS_RX_TOTAL
+    log = logging.getLogger('stats')
+    log.info("ACARS messages received (last {n} mins / lifetime): {last5m}/{life}".format(
+        n = int(trigger_in/60),
+        last5m = COUNTER_ACARS_MSGS_RX_LAST,
+        life = COUNTER_ACARS_MSGS_RX_TOTAL,
+    ))
+    COUNTER_ACARS_MSGS_RX_LAST = 0
+    log.info("VDLM2 messages received (last {n} mins / lifetime): {last5m}/{life}".format(
+        n = int(trigger_in/60),
+        last5m = COUNTER_VDLM2_MSGS_RX_LAST,
+        life = COUNTER_VDLM2_MSGS_RX_TOTAL,
+    ))
+    COUNTER_VDLM2_MSGS_RX_LAST = 0
+    task.deferLater(reactor, trigger_in, log_counters, trigger_in)
+    return None
 
 class MessageBuffer:
     """
@@ -71,15 +108,11 @@ class UDPSender():
         self,
         data: bytes,
     ):
-        print("sending to {host}:{port}".format(
-            host=self.host,
-            port=self.port,
-        ))
         # TODO: implement a back-off. If sending fails, don't retry for X seconds
         try:
             self.sock.sendto(data, (self.host, self.port))
         except:
-            log.failure("Failed sending to {host}:{port}/udp".format(
+            log.error("Error sending to {host}:{port}/udp".format(
                 host=self.host,
                 port=self.port,
             ))
@@ -105,8 +138,11 @@ class ACARS(DatagramProtocol):
         self.message_buffer = message_buffer
 
     def datagramReceived(self, data, addr):
-        print("ACARS: received {!r} from {}".format(data, addr))
+        global COUNTER_ACARS_MSGS_RX_LAST
+        global COUNTER_ACARS_MSGS_RX_TOTAL
         self.message_buffer.append(data)
+        COUNTER_ACARS_MSGS_RX_TOTAL += 1
+        COUNTER_ACARS_MSGS_RX_LAST += 1
 
 class VDLM2(DatagramProtocol):
     """
@@ -129,16 +165,104 @@ class VDLM2(DatagramProtocol):
         self.message_buffer = message_buffer
 
     def datagramReceived(self, data, addr):
-        print("VDLM2: received {!r} from {}".format(data, addr))
-        self.transport.write(data, addr)
+        global COUNTER_VDLM2_MSGS_RX_LAST
+        global COUNTER_VDLM2_MSGS_RX_TOTAL
         self.message_buffer.append(data)
+        COUNTER_VDLM2_MSGS_RX_TOTAL += 1
+        COUNTER_VDLM2_MSGS_RX_LAST += 1
+        
+def split_env_safely(
+    env: str,
+    sep: str=';',
+):
+    """
+    Splits the contents of environment variable `env` with separator `sep`.
+    If an exception occurs, return an empty list.
+    """
+    try:
+        return os.getenv(env).split(sep)
+    except:
+        return list()
+    return list()
 
-acars_mb = MessageBuffer()
-acars_mb.add_udp_client("192.168.69.35",5550)
 
-vdlm2_mb = MessageBuffer()
-vdlm2_mb.add_udp_client("192.168.69.35",5555)
+if __name__ == "__main__":
 
-reactor.listenUDP(25550, ACARS(acars_mb))
-reactor.listenUDP(25555, VDLM2(vdlm2_mb))
-reactor.run()
+    # Command line / OS Env
+    parser = argparse.ArgumentParser(
+        description='Route ACARS/VDLM2 messages'
+    )
+    parser.add_argument(
+        '-lau', '--listen-udp-acars',
+        help='UDP port to listen for ACARS messages. Can be specified multiple times to listen on multiple ports. (default: 5550)',
+        type=str,
+        nargs='*',
+        default=os.getenv('AR_LISTEN_UDP_ACARS', "5550").split(';'),
+    )
+    parser.add_argument(
+        '-lvu', '--listen-udp-vdlm2',
+        help='UDP port to listen for VDLM2 messages. Can be specified multiple times to listen on multiple ports. (default: 5555)',
+        type=str,
+        nargs='*',
+        default=os.getenv('AR_LISTEN_UDP_VDLM2', "5555").split(';'),
+    )
+    parser.add_argument(
+        '-sua', '--send-udp-acars',
+        help='Client to send ACARS messages in host:port format. Can be specified multiple times to send to multiple clients.',
+        type=str,
+        nargs='*',
+        default=split_env_safely('AR_SEND_UDP_ACARS'),
+    )
+    parser.add_argument(
+        '-suv', '--send-udp-vdlm2',
+        help='Client to send VDLM2 messages in host:port format. Can be specified multiple times to send to multiple clients.',
+        type=str,
+        nargs='*',
+        default=split_env_safely('AR_SEND_UDP_VDLM2'),
+    )
+    parser.add_argument(
+        '--stats-every',
+        help='Print statistics every N minutes (default: 5)',
+        type=int,
+        nargs='?',
+        default=5,
+    )
+    args = parser.parse_args()
+
+    # Set up message buffers
+    acars_mb = MessageBuffer()
+    vdlm2_mb = MessageBuffer()
+
+    # Handle `--send-udp-acars` (set up ACARS UDP senders)
+    for c in args.send_udp_acars:
+        log.info("Forwarding ACARS UDP datagrams to: {client}".format(
+            client=c,
+        ))
+        acars_mb.add_udp_client(c.split(':')[0], int(c.split(':')[1]))
+
+    # Handle `--send-udp-vdlm2` (set up VDLM2 UDP senders)
+    for c in args.send_udp_acars:
+        log.info("Forwarding VDLM2 UDP datagrams to: {client}".format(
+            client=c,
+        ))
+        vdlm2_mb.add_udp_client(c.split(':')[0], int(c.split(':')[1]))
+
+    # Handle `--listen-udp-acars` (set up ACARS listeners)
+    for s in args.listen_udp_acars:
+        log.info("Listening for ACARS UDP datagrams on port: {port}".format(
+            port=s,
+        ))
+        reactor.listenUDP(int(s), ACARS(acars_mb))
+
+    # Handle `--listen-udp-vdlm2` (set up VDLM2 listeners)
+    for s in args.listen_udp_vdlm2:
+        log.info("Listening for VDLM2 UDP datagrams on port: {port}".format(
+            port=s,
+        ))
+        reactor.listenUDP(int(s), VDLM2(vdlm2_mb))
+
+    # Set up stats logging
+    task.deferLater(reactor, args.stats_every * 60, log_counters, args.stats_every * 60)
+
+    # Run!
+    reactor.run()
