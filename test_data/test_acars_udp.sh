0d2f1411b6e74209c7ea396a7c03119b67b8e2e6
Mike Nye <mike.nye@gmail.com>
2022-02-20 03:43:11 +0800

    testing

--- test_data/test_acars_udp.sh
+++ test_data/test_acars_udp.sh
@@ -8,9 +8,9 @@ sleep 2
 python3 ./acars_router/acars_router.py -v --listen-udp-acars 5550 --send-udp-acars 127.0.0.1:15550 &
 sleep 2
 
-while read x; do
-  echo "$x" | nc -u -v -q 1 -w 5 127.0.0.1 5550
-done < ./test_data/acars.patched
+while IFS="" read -r p || [ -n "$p" ]
+  printf '%s\n' "$p" | nc -u -v -q 1 -w 5 127.0.0.1 5550
+done <./test_data/acars.patched
 
 sleep 2
 cat ./tmp/acars.udp.out
