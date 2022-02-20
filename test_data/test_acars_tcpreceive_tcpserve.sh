60acb13d785ce3d4025fb5ed1d12dffe33fba67e
Mike Nye <mike.nye@gmail.com>
2022-02-20 16:39:19 +0800

    test tcpreceive/tcpserve

---
+++ test_data/test_acars_tcpreceive_tcpserve.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+set -xe
+
+# Start acars_router
+timeout 10s python3 ./acars_router/acars_router.py -vv --skew-window 30 --receive-tcp-acars=127.0.0.1:15550 --serve-tcp-acars 5550 &
+sleep 1
+
+# Start fake destination server for acars_router output
+socat -d -t5 TCP:127.0.0.1:5550,fork OPEN:/tmp/acars.tcpreceive.tcpsserve.out,creat,append &
+sleep 1
+
+# Start fake source server 
+socat -ddd -x -t5 TCP-LISTEN:15550 EXEC:"./test_data/send_lines.sh ./test_data/acars.patched"
+sleep 1
+
+jq . < /tmp/acars.tcpreceive.tcpsserve.out 
+
+# # Start fake destination server for reference output
+# socat -d -t5 TCP-LISTEN:25550,fork OPEN:/tmp/acars.tcplisten.tcpsend.out.reference,creat,append &
+# sleep 1
+
+# # Send data bypassing acars_router
+# while IFS="" read -r p || [ -n "$p" ]; do
+#     printf '%s\n' "$p" | socat - TCP:127.0.0.1:25550;
+# done <./test_data/acars.patched
+
+# # Start fake destination server for acars_router output
+# socat -d -t5 TCP-LISTEN:15550,fork OPEN:/tmp/acars.tcplisten.tcpsend.out,creat,append &
+# sleep 1
+
+
+# # Send test data thru acars_router
+# while IFS="" read -r p || [ -n "$p" ]; do
+#     printf '%s\n' "$p" | socat - TCP:127.0.0.1:5550;
+# done <./test_data/acars.patched
+
+# # Re-format output files
+# jq -M . < /tmp/acars.tcplisten.tcpsend.out.reference > /tmp/acars.tcplisten.tcpsend.out.reference.reformatted
+# jq -M . < /tmp/acars.tcplisten.tcpsend.out > /tmp/acars.tcplisten.tcpsend.out.reformatted
+
+# # Check output
+# diff /tmp/acars.tcplisten.tcpsend.out.reference.reformatted /tmp/acars.tcplisten.tcpsend.out.reformatted
+# exit $?
