7c4ec921364f1c62dfc8eb5030581833723c6901
Mike Nye <mike.nye@gmail.com>
2022-02-20 13:55:52 +0800

    skew window for testing

--- test_data/test_acars_udp.sh
+++ test_data/test_acars_udp.sh
@@ -5,7 +5,7 @@ set -x
 socat UDP-LISTEN:15550 CREATE:/tmp/acars.udp.out &
 SOCAT_PID="$!"
 sleep 2
-python3 ./acars_router/acars_router.py -v --listen-udp-acars 5550 --send-udp-acars 127.0.0.1:15550 &
+python3 ./acars_router/acars_router.py -v --skew-window 30 --listen-udp-acars 5550 --send-udp-acars 127.0.0.1:15550 &
 sleep 2
 
 while IFS="" read -r p || [ -n "$p" ]; do
