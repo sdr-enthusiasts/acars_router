ae07ef34f39fdcb4d4422f2d033b82fe7d90bf7f
Mike Nye <mike.nye@gmail.com>
2022-02-20 14:00:21 +0800

    troubleshoot socat

--- test_data/test_acars_udp.sh
+++ test_data/test_acars_udp.sh
@@ -2,7 +2,7 @@
 
 set -x
 
-socat UDP-LISTEN:15550 CREATE:/tmp/acars.udp.out &
+socat UDP-LISTEN:15550 OPEN:/tmp/acars.udp.out,creat,append &
 SOCAT_PID="$!"
 sleep 2
 python3 ./acars_router/acars_router.py -v --skew-window 30 --listen-udp-acars 5550 --send-udp-acars 127.0.0.1:15550 &
