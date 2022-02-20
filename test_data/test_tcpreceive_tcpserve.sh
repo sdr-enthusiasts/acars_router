95e6ea0f7980a80959f0a65ee7209407b4e18428
Mike Nye <mike.nye@gmail.com>
2022-02-20 18:50:57 +0800

    finalise testing

--- test_data/test_tcpreceive_tcpserve.sh
+++ test_data/test_tcpreceive_tcpserve.sh
@@ -19,29 +19,10 @@ sleep 10
 
 jq . < /tmp/"$1".tcpreceive.tcpsserve.out 
 
-# # Start fake destination server for reference output
-# socat -d -t5 TCP-LISTEN:25550,fork OPEN:/tmp/acars.tcplisten.tcpsend.out.reference,creat,append &
-# sleep 1
+# Re-format output files
+jq -M . < ./test_data/"$1".patched > /tmp/"$1".tcpreceive.tcpsserve.out.reference.reformatted
+jq -M . < /tmp/"$1".tcpreceive.tcpsserve.out > /tmp/"$1".tcpreceive.tcpsserve.out.reformatted
 
-# # Send data bypassing acars_router
-# while IFS="" read -r p || [ -n "$p" ]; do
-#     printf '%s\n' "$p" | socat - TCP:127.0.0.1:25550;
-# done <./test_data/acars.patched
-
-# # Start fake destination server for acars_router output
-# socat -d -t5 TCP-LISTEN:15550,fork OPEN:/tmp/acars.tcplisten.tcpsend.out,creat,append &
-# sleep 1
-
-
-# # Send test data thru acars_router
-# while IFS="" read -r p || [ -n "$p" ]; do
-#     printf '%s\n' "$p" | socat - TCP:127.0.0.1:5550;
-# done <./test_data/acars.patched
-
-# # Re-format output files
-# jq -M . < /tmp/acars.tcplisten.tcpsend.out.reference > /tmp/acars.tcplisten.tcpsend.out.reference.reformatted
-# jq -M . < /tmp/acars.tcplisten.tcpsend.out > /tmp/acars.tcplisten.tcpsend.out.reformatted
-
-# # Check output
-# diff /tmp/acars.tcplisten.tcpsend.out.reference.reformatted /tmp/acars.tcplisten.tcpsend.out.reformatted
-# exit $?
+# Check output
+diff /tmp/"$1".tcpreceive.tcpsserve.out.reference.reformatted /tmp/"$1".tcpreceive.tcpsserve.out.reformatted
+exit $?
