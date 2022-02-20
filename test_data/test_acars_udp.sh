948f068de2f018116c2aa07aa50862b472384ca4
Mike Nye <mike.nye@gmail.com>
2022-02-20 14:02:38 +0800

    verbosity

--- test_data/test_acars_udp.sh
+++ test_data/test_acars_udp.sh
@@ -5,7 +5,7 @@ set -x
 socat UDP-LISTEN:15550 OPEN:/tmp/acars.udp.out,creat,append &
 SOCAT_PID="$!"
 sleep 2
-python3 ./acars_router/acars_router.py -v --skew-window 30 --listen-udp-acars 5550 --send-udp-acars 127.0.0.1:15550 &
+python3 ./acars_router/acars_router.py -vv --skew-window 30 --listen-udp-acars 5550 --send-udp-acars 127.0.0.1:15550 &
 sleep 2
 
 while IFS="" read -r p || [ -n "$p" ]; do
