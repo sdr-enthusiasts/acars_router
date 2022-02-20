a24d1bbbefa9ee5f3127778385db57136a8f723d
Mike Nye <mike.nye@gmail.com>
2022-02-20 16:44:01 +0800

    change ports

--- test_data/test_acars_tcpreceive_tcpserve.sh
+++ test_data/test_acars_tcpreceive_tcpserve.sh
@@ -2,15 +2,15 @@
 set -xe
 
 # Start acars_router
-timeout 10s python3 ./acars_router/acars_router.py -vv --skew-window 30 --receive-tcp-acars=127.0.0.1:15550 --serve-tcp-acars 5550 &
+timeout 10s python3 ./acars_router/acars_router.py -vv --skew-window 30 --receive-tcp-acars=127.0.0.1:25550 --serve-tcp-acars 5552 &
 sleep 1
 
 # Start fake destination server for acars_router output
-socat -d -t5 TCP:127.0.0.1:5550,fork OPEN:/tmp/acars.tcpreceive.tcpsserve.out,creat,append &
+socat -d -t5 TCP:127.0.0.1:5552,fork OPEN:/tmp/acars.tcpreceive.tcpsserve.out,creat,append &
 sleep 1
 
 # Start fake source server 
-socat -ddd -x -t5 TCP-LISTEN:15550 EXEC:"./test_data/send_lines.sh ./test_data/acars.patched"
+socat -ddd -x -t5 TCP-LISTEN:25550 EXEC:"./test_data/send_lines.sh ./test_data/acars.patched"
 sleep 1
 
 jq . < /tmp/acars.tcpreceive.tcpsserve.out 
