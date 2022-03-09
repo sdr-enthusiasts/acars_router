1e1148703cc0c8c31680993a3462a2d2dc6e3856
Mike Nye <mike.nye@gmail.com>
2022-03-09 13:41:01 +0800

    fix bug

--- acars_router/acars_router.py
+++ acars_router/acars_router.py
@@ -1279,7 +1279,7 @@ def valid_args(args):
             return False
         # ensure port valid
         try:
-            if not valid_tcp_udp_port(port):
+            if not valid_tcp_udp_port(int(port)):
                 raise ValueError
         except ValueError:
             logger.critical(f"receive_tcp_vdlm2: invalid port: {port}")
