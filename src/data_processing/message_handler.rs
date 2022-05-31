cf2b58bb47adfac9783a478df8406c76d38c8169
Fred Clausen <43556888+fredclausen@users.noreply.github.com>
2022-05-31 12:36:05 -0600

    hacky implementation of inserting the app fields for proxying
    This is not the final place this will be done.
    
    * Needs to respect the command line flag to disable proxying

--- src/data_processing/message_handler.rs
+++ src/data_processing/message_handler.rs
@@ -9,7 +9,34 @@ use log::debug;
 use tokio::sync::mpsc::Receiver;
 
 pub async fn watch_message_queue(mut queue: Receiver<serde_json::Value>) {
-    while let Some(message) = queue.recv().await {
+    while let Some(mut message) = queue.recv().await {
         debug!("GOT: {}", message);
+
+        match message["vdl2"].get("app") {
+            // dumpvdl2 message
+            Some(_) => {
+                debug!("dumpvdl2 message");
+                message["vdl2"]["app"]["proxied"] = serde_json::Value::Bool(true);
+                message["vdl2"]["app"]["proxied_by"] =
+                    serde_json::Value::String("acars_router".to_string());
+            }
+            // acarsdec or vdlm2dec message
+            None => match message.get("app") {
+                Some(_) => {
+                    message["app"]["proxied"] = serde_json::Value::Bool(true);
+                    message["app"]["proxied_by"] =
+                        serde_json::Value::String("acars_router".to_string());
+                }
+                None => {
+                    // insert app in to message
+                    message["app"] = serde_json::Value::Object(serde_json::map::Map::new());
+                    message["app"]["proxied"] = serde_json::Value::Bool(true);
+                    message["app"]["proxied_by"] =
+                        serde_json::Value::String("acars_router".to_string());
+                }
+            },
+        }
+
+        debug!("SENDING: {}", message);
     }
 }
